c6c2554bb049755caf4181b698caafa211cc9f74
Anthony Ware <aware91coding@gmail.com>
2020-03-20 20:32:47 -0500

    working on tabs and have the set up.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,14 +8,28 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
-axios
-    .get('https://lambda-times-backend.herokuapp.com/topics')
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
     .then(response => {
-    console.log('API RESPONSE', response) 
+    console.log('Tabs Data', response) 
+    response.data.forEach( item => {
+        const para = (item);
+        parent.appendChild(button);
     })
 
     .catch(err => {
         console.log('Error', err)
     })
 
-    
+    function Tabs(tabs){
+        const topics = document.createElement('div')
+        const title = document.createElement('span')
+
+        topics.classList.add('topics')
+        title.classList.add('title')
+        
+        topics.appendChild(title)
+
+        title.textContent = `TRENDING TOPICS: ${tabs}`
+
+        return Tabs
+    }
